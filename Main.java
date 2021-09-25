993a5b958f7a415c473fc0a31ebbb2e24a02d555
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-09-26 01:34:11 +0530

    Command line argument that prints all the value

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.aayush.gulia;
+
+public class Main {
+    // Command line argument that prints all the value
+
+    public static void main(String[] args) {
+	for (int i=0; i< args.length; i++){
+        System.out.println(args[i]);
+    }
+    }
+}

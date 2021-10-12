b62f76407ae3d30440179111fd7b453d2c38acd1
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-10-12 22:23:31 +0530

    Example_Parameterised constructor
    super method, non param constructors and param constructors example.

---
+++ Inheritance/Parameterized_Constructor/Rectangle.java
@@ -0,0 +1,31 @@
+package Inheritance.param;
+
+public class Rectangle {
+    int length,breadth;
+    public Rectangle(){
+        length=breadth=1;
+    }
+    public Rectangle(int len, int br){
+        length=len;
+        breadth=br;
+    }
+
+    public static void main(String[] args) {
+        //Rectangle rectangle = new Rectangle(10,20); // these values will be overwritten when cuboid constructor will be called.
+        Cuboid cuboid = new Cuboid(10,20,10);
+        System.out.println(cuboid.volume());
+    }
+}
+class Cuboid extends Rectangle{
+    int height;
+    public Cuboid(){
+        height=1;
+    }
+    public Cuboid(int len, int br, int h){
+        super(len,br); // super method passed values to parent class(super class). length & breadth to rectangle class
+        height=h;
+    }
+    public int volume(){
+        return length*breadth*height;
+    }
+}

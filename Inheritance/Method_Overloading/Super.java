dc356a234285db6254c92ff6a197fe5b676267d7
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-10-13 22:29:19 +0530

    Example_Method_Overloading

---
+++ Inheritance/Method_Overloading/Super.java
@@ -0,0 +1,37 @@
+package Method_Overloading;
+/*
+    @author: Aayush Gulia
+    @email: aayushgulia07@outlook.com
+redefining the method of super class in sub class.
+In this program there are two void methods Display() with same name and same signature, it's called
+method overriding not overloading.
+ */
+
+public class Super {
+    public Super(){
+
+    }
+    void Display(){
+        System.out.println("Display method of super class.");
+    }
+}
+
+class Child extends Super{
+    public Child(){
+
+    }
+    void Display(){
+        super.Display(); // this calls method of it's super class.
+        System.out.println("Display method of child class");
+        /*
+        This Display method is override in it's sub class.
+         */
+    }
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.Display();
+//        Super s = new Super();
+//        s.Display();
+    }
+}

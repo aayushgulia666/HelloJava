8e9e580875be135aaccf4b5a2d19a04a393d7542
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-10-24 22:02:29 +0530

    Multi threading using Runnable interface

---
+++ MultiThreading/MultiThreading_Runnable.java
@@ -0,0 +1,32 @@
+package MultiThreading;
+/*
+    Multi threading using runnable interface.
+
+    it is mandatory to override run method with interface Runnable.
+ */
+
+public class MultiThreading_Runnable implements Runnable{
+    @Override
+    public void run() {
+        Display();
+    }
+
+    public void Display(){
+        int i=0;
+        while(true){
+            System.out.println("Hello : " + i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        MultiThreading_Runnable multiThreading_runnable = new MultiThreading_Runnable();
+        Thread t = new Thread(multiThreading_runnable); // passed the object of the class whose give it's own thread.
+        t.start();
+        int i=0;
+        while(true){
+            System.out.println("World : " + i);
+            i++;
+        }
+    }
+}

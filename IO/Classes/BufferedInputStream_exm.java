6b56a98a99f0af8e9c178c0b402d9e9ebf56aaf9
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-12-06 22:15:18 +0530

    BufferedInputStream
    Buffered Input Stream example

---
+++ IO/Classes/BufferedInputStream_exm.java
@@ -0,0 +1,19 @@
+package Final_Production;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+/*
+    @author: aayushgulia07@outlook.com
+ */
+
+public class BufferedInputStream_exm {
+    public static void main(String[] args) throws Exception{
+        FileInputStream fileInputStream = new FileInputStream("C:\\CopyUpperToLower\\src\\Final_Production\\Text.txt");
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+        int i;
+        while((i= bufferedInputStream.read())!=-1){
+            System.out.print((char) i);
+        }
+    }
+}

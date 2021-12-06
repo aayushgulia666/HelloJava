26327e7d5d208867f5f181e5d4471bf8d0090bc9
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-12-06 22:26:59 +0530

    ByteArrayOutputStream
    Byte Array Output Stream example.

---
+++ IO/Classes/ByteArrayOutputStream_exm.java
@@ -0,0 +1,28 @@
+package Final_Production;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+/*
+    ByteArrayOutputStream - is used to write common data into multiple files,
+    in this stream data is written into a byte array which can be written into
+    multiple streams later.
+
+    @author: aayushgulia07@outlook.com
+
+ */
+public class ByteArrayOutputStream_exm {
+    public static void main(String[] args) throws Exception{
+        FileOutputStream fileOutputStream1 = new FileOutputStream("C:\\CopyUpperToLower\\src\\Final_Production\\Text.txt");
+        FileOutputStream fileOutputStream2 = new FileOutputStream("C:\\CopyUpperToLower\\src\\Final_Production\\Test.txt");
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        String str = "This is an example of ByteArrayOutputStream";
+        byte[] bytes = str.getBytes();
+
+        byteArrayOutputStream.write(bytes);
+
+        byteArrayOutputStream.writeTo(fileOutputStream1);
+        byteArrayOutputStream.writeTo(fileOutputStream2);
+    }
+}

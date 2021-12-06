91119164cca5078fe403718f63dc47dc3e8756c2
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2021-12-06 22:56:07 +0530

    DataOutputStream
    Data Output Stream example.

---
+++ IO/DataOutputStream_exm.java
@@ -0,0 +1,21 @@
+package Final_Production;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+/*
+    DataOutputStream: used to write data which can be read by DataInputStream in a machine independent way.
+
+    @author: aayushgulia07@outlook.com
+ */
+
+public class DataOutputStream_exm {
+    public static void main(String[] args) throws Exception{
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\CopyUpperToLower\\src\\Final_Production\\Text.txt");
+        DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
+
+        String str = "This is an example of DataOutputStream";
+        byte[] bytes = str.getBytes();
+
+        dataOutputStream.write(bytes);
+    }
+}

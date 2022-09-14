6b9d87e51cd8dde2352e581000bd0e67d0f58c3e
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2022-09-14 21:33:18 +0530

    Print half pyramids

---
+++ PrintingPatterns/HalfPyramids.java
@@ -0,0 +1,25 @@
+package com.aayush.gulia.Patterns;
+
+public class HalfPyramids {
+    static void halfPyramid(int n){
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i=n; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        halfPyramid(7);
+    }
+}

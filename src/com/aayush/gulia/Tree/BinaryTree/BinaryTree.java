655c718d0c543512c9020eb0f10bb7aee8ecf068
Aayush Gulia <aayushgulia07@outlook.com>
2022-09-29 14:28:30 +0530

    Binary tree implemented.
    Using linked representation and queue data structure.

--- src/com/aayush/gulia/Tree/BinaryTree/BinaryTree.java
+++ src/com/aayush/gulia/Tree/BinaryTree/BinaryTree.java
@@ -1,4 +1,97 @@
 package com.aayush.gulia.Tree.BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * This class implements a binary tree using linked representation with the help of a queue data structure.
+ *
+ * @author Aayush Gulia <aayushgulia07@outlook.com>
+ * @since 29/09/2022
+ * @see <a href="https://en.wikipedia.org/wiki/Binary_tree">Binary Tree</a>
+ */
+
 public class BinaryTree {
+
+    /**
+     * Global references.
+     */
+
+    static Node root;
+    static Queue<Node> queue = new LinkedList<Node>();
+    static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Node class defines the structure of nodes in binary tree.
+     */
+
+    static class Node{
+        Node lChild;
+        int data;
+        Node rChild;
+
+        /**
+         * Creates a new node whenever this constructor is called.
+         * @param lChild reference to left child of binary tree.
+         * @param data data stored in the node.
+         * @param rChild reference to right child of binary tree.
+         */
+
+        Node(Node lChild, int data, Node rChild){
+            this.lChild = lChild;
+            this.data = data;
+            this.rChild = rChild;
+        }
+    }
+
+    /**
+     * Creates a binary tree.
+     */
+
+    public static void createBinaryTree(){
+        Node p, t;
+        int x;
+
+        // create root node.
+
+        System.out.println("Enter root value: ");
+        x = scanner.nextInt();
+        root = new Node(null, x, null);
+
+        queue.add(root);
+
+        // keep adding elements until the queue is empty.
+
+        while (!queue.isEmpty()){
+
+            p = queue.remove();
+
+            // Left child.
+
+            System.out.println("Enter left child: ");
+            x = scanner.nextInt();
+
+            if (x != -1){ // -1 --> node does not exist.
+                t = new Node(null, x, null);
+                p.lChild = t;
+                queue.add(t);
+            }
+
+            // Right child.
+
+            System.out.println("Enter right child: ");
+            x = scanner.nextInt();
+
+            if (x != -1){
+                t = new Node(null, x, null);
+                p.rChild = t;
+                queue.add(t);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        createBinaryTree();
+    }
 }

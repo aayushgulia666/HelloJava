edb2653ea66ffc4e075970cfb386b6a79d3f4e2e
Aayush Gulia <aayushgulia07@outlook.com>
2022-09-29 13:56:34 +0530

    BinaryTree.java added.

--- src/com/aayush/gulia/Hashing/ClosedHashing/OpenAddressing/DoubleHashing.java
+++ src/com/aayush/gulia/Hashing/ClosedHashing/OpenAddressing/DoubleHashing.java
@@ -1,5 +1,58 @@
 package com.aayush.gulia.Hashing.ClosedHashing.OpenAddressing;
 
 public class DoubleHashing {
+    static int[] hashtable;
+    static int SIZE = 10;
+    static int PRIME = 7;
+    static int entries;
 
+    public static void initializeHashTable(){
+        hashtable = new int[SIZE];
+    }
+
+    public static int hashFunction(int element){
+        return element % SIZE;
+    }
+
+    public static int primeHash(int key){
+        return PRIME - (key % PRIME);
+    }
+
+    public static int doubleHash(int element){
+        int index = hashFunction(element);
+
+        int i = 0;
+        while (hashtable[(index + i * primeHash(index)) % SIZE] != 0 ){
+           i++;
+        }
+
+        return (index + i * primeHash(index)) % SIZE;
+    }
+
+    public static void insertElement(int element){
+        int index = hashFunction(element);
+
+        if (hashtable[index] != 0){
+            index = doubleHash(element);
+        }
+
+        hashtable[index] = element;
+    }
+
+    public static float loadFactor(){
+        float lf = 0;
+        if (entries != 0){
+            lf = (float) entries/SIZE;
+        }
+        return lf;
+    }
+
+    public static void main(String[] args) {
+        initializeHashTable();
+        insertElement(34);
+        insertElement(34);
+        insertElement(34);
+        insertElement(34);
+        insertElement(34);
+    }
 }
---
+++ src/com/aayush/gulia/Tree/BinaryTree/BinaryTree.java
@@ -0,0 +1,4 @@
+package com.aayush.gulia.Tree.BinaryTree;
+
+public class BinaryTree {
+}

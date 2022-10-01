e3d2e42a1a41cbadcc82e68725ec16ff5d97fc0c
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-02 00:01:27 +0530

    selectionSort() added.

--- src/com/aayush/gulia/Sorting/ComparisionBasedSorts/SelectionSort/SelectionSort.java
+++ src/com/aayush/gulia/Sorting/ComparisionBasedSorts/SelectionSort/SelectionSort.java
@@ -1,4 +1,37 @@
 package com.aayush.gulia.Sorting.ComparisionBasedSorts.SelectionSort;
 
+import java.util.Arrays;
+
+/**
+ * This class implements selection sort.
+ *
+ * Kth pass gives Kth small element.
+ *
+ * @author Aayush Gulia <aayushgulia07@outlook.com>
+ * @see <a href="https://www.geeksforgeeks.org/selection-sort/">Selection sort</a>
+ */
+
 public class SelectionSort {
+
+    /**
+     * Performs selection sort on an array.
+     * @param a array on which selection sort is performed.
+     */
+    public static void selectionSort(int[] a){
+        int i,j,k;
+        int temp;
+
+        for (i=0; i<a.length-1; i++){
+            for (j=k=i; j<a.length; j++){
+                if (a[j] < a[k]){
+                    k=j;
+                }
+            }
+            temp = a[i];
+            a[i] = a[k];
+            a[k] = temp;
+        }
+
+        System.out.println(Arrays.toString(a));
+    }
 }

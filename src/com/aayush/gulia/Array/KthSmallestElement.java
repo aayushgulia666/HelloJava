6895777ddb2be9df5568cae414b903dc28390f30
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-02 00:13:56 +0530

    KthSmallestElement.java created and method added.

---
+++ src/com/aayush/gulia/Array/KthSmallestElement.java
@@ -0,0 +1,32 @@
+package com.aayush.gulia.Array;
+
+/**
+ * Selection sort gives the Kth smallest element.
+ */
+public class KthSmallestElement {
+    /**
+     * Returns the Kth smallest element.
+     * @param a array of elements.
+     * @param passes gives the smallest element. i.,e Kth pass will give Kth small element.
+     * @return Kth smallest element.
+     */
+    public static int kThSmallestElement(int[] a, int passes){
+        int i,j,k;
+        int temp;
+
+        if (passes == 0)
+            return 0;
+
+        for (i=0; i<passes; i++){
+            for (j=k=i; j<a.length; j++){
+                if (a[j] < a[k]){
+                    k=j;
+                }
+            }
+            temp = a[i];
+            a[i] = a[k];
+            a[k] = temp;
+        }
+        return a[passes-1];
+    }
+}

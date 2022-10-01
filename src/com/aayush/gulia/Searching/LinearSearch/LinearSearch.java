687df3532bde775fedd6162a830dea9dd7fadb88
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-01 13:28:52 +0530

    Linear search added.

--- src/com/aayush/gulia/Searching/LinearSearch/LinearSearch.java
+++ src/com/aayush/gulia/Searching/LinearSearch/LinearSearch.java
@@ -1,4 +1,25 @@
 package com.aayush.gulia.Searching.LinearSearch;
 
+/**
+ * This class implements linear search.
+ *
+ * @since 01/10/2022
+ */
+
 public class LinearSearch {
+
+    /**
+     * Search the element in the array and return the index if element is found, else return -1.
+     * @param a an array of elements.
+     * @param key element to search in the array.
+     * @return index of element if found else -1.
+     */
+    public static int linearSearch(int[] a, int key){
+        for (int i=0; i<a.length; i++){
+            if (a[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
 }

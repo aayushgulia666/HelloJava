16394a31899b3e53a3e92ab177288989714027b2
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-01 13:55:01 +0530

    findPeakElement() method added.

---
+++ src/com/aayush/gulia/Array/FindingPeakElement.java
@@ -0,0 +1,50 @@
+package com.aayush.gulia.Array;
+
+/**
+ * This class finds a peak element in an array using binary search.
+ * @since 01/10/2022
+ */
+public class FindingPeakElement {
+    /**
+     * Returns peak element if exist.
+     * @param a array of elements
+     * @return index of peak element.
+     */
+
+    public static int findPeakElement(int[] a){
+        int low = 0;
+        int high = a.length-1;
+        int mid;
+
+        while (low <= high){
+            mid = low + ((high - low) / 2);
+
+            if (mid > 0 && mid < a.length-1){
+                if (a[mid] > a[mid - 1] && a[mid] > a[mid + 1]){
+                    return mid;
+                }
+
+                else if(a[mid + 1] > a[mid]){
+                    low = mid + 1;
+                }
+
+                else{
+                    high = mid - 1;
+                }
+            }
+
+            else if (mid == 0){
+                if (a[mid] > a[mid + 1]){
+                    return mid;
+                }
+            }
+
+            else if (mid == a.length - 1){
+                if (a[mid] > a[a.length - 2]){
+                    return mid;
+                }
+            }
+        }
+        return -1;
+    }
+}

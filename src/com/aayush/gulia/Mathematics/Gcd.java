c671028cce11e3446747818a4d257007a1e179e0
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-05 13:52:46 +0530

    Gcd.java added.

---
+++ src/com/aayush/gulia/Mathematics/Gcd.java
@@ -0,0 +1,9 @@
+package com.aayush.gulia.Mathematics;
+
+public class Gcd {
+    public static int gcd(int a, int b){
+        if (b==0)
+            return a;
+        return gcd(b, a%b);
+    }
+}

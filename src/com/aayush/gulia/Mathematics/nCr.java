23e53c278ae2017a6326f7c435de09d126bf9435
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-13 17:46:18 +0530

    nCr.java added.

---
+++ src/com/aayush/gulia/Mathematics/nCr.java
@@ -0,0 +1,20 @@
+package com.aayush.gulia.Mathematics;
+
+public class nCr {
+    public static int factorial(int n){
+        if (n == 0 || n == 1){
+            return 1;
+        }
+
+        int result = 1;
+        for (int i=2; i<=n; i++){
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int nCr(int n, int r){
+        return factorial(n)/(factorial(r)*factorial(n-r));
+    }
+
+}

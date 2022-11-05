5ad85103c649d7267bb468b7d6547664a5a56736
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-05 13:15:04 +0530

    PascalTriangle.java updated.

--- src/com/aayush/gulia/Questions/PascalTriangle.java
+++ src/com/aayush/gulia/Questions/PascalTriangle.java
@@ -1,5 +1,8 @@
 package com.aayush.gulia.Questions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangle {
 
     public static int factorial(int n){
@@ -18,7 +21,7 @@ public class PascalTriangle {
         return factorial(n)/(factorial(r)*factorial(n-r));
     }
 
-    public static void printPascalTriangle(int n){
+    public static void printPascalTriangleRow(int n){
         for (int i=0; i<n; i++){
             for (int j=0; j<n-i; j++){
                 System.out.print(" ");
@@ -31,8 +34,31 @@ public class PascalTriangle {
         }
     }
 
-    public static void main(String[] args) {
-        printPascalTriangle(5);
+    public static void printPascalTriangle(int n){
+
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> row, prev = null;
+
+        for (int i=0; i<n; i++){
+            row = new ArrayList<>();
+            for (int j=0; j<=i; j++){
+                if (j==0 || j == i){
+                    row.add(1);
+                }
+                else{
+                    //row.add(prev.get(j - 1) + prev.get(j));
+                    row.add(prev.get(j-1) + prev.get(j));
+                }
+            }
+            prev = row;
+            answer.add(row);
+        }
+
+        System.out.println(answer);
     }
 
+    public static void main(String[] args) {
+        printPascalTriangle(5);
+
+    }
 }

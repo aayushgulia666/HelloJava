c0042076edaa3e69cf6a634a210c017eed5de224
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-05 23:04:44 +0530

    RotateImage.java updated.
    More efficient approach used.

--- src/com/aayush/gulia/Array/Questions/RotateImage.java
+++ src/com/aayush/gulia/Array/Questions/RotateImage.java
@@ -2,14 +2,44 @@ package com.aayush.gulia.Array.Questions;
 
 /**
  * @author Aayush Gulia <aayushgulia07@outlook.com>
+ * @since 05/11/2022
  * leet code 48
  */
 public class RotateImage {
     public static void main(String[] args) {
-        int[][] image = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
-        int[][] rotatedImage = copyRotatedImage(image);
-        displayImage(rotatedImage);
+        int[][] image = {{1,2,3}, {4,5,6}, {7,8,9}};
+        rotate(image);
+        displayImage(image);
+    }
 
+    /**
+     * Rotates the image by 90°
+     *
+     * T:O(n^2)
+     * S:O(1)
+     * @param matrix input 2D array
+     */
+
+    private static void rotate(int[][] matrix){
+        // transpose and then reverse.
+
+        for (int i=0; i<matrix.length; i++){
+            for (int j=i; j<matrix[0].length; j++){
+                int temp;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j< matrix.length/2; j++){
+                int temp;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
     }
 
     /**

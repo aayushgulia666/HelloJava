2da5de2dd5ecc4c283775f654bb4d676a3fbf3a0
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-13 14:59:40 +0530

    List.java added in package LinkedList.

---
+++ src/com/aayush/gulia/LinkedList/List.java
@@ -0,0 +1,38 @@
+package com.aayush.gulia.LinkedList;
+
+public class List {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+
+        Node(){}
+
+    }
+    public static Node createSinglyLinkedList(int[] a){
+        Node first, last, temp;
+
+        first = new Node(a[0], null);
+        last=first;
+        for (int i=1; i<a.length; i++){
+            temp = new Node(a[i], null);
+            last.next = temp;
+            last = temp;
+        }
+
+        return first;
+    }
+
+    public static void display(Node first){
+        Node p = first;
+        while (p!=null){
+            System.out.print(p.data + " ");
+            p=p.next;
+        }
+        System.out.println();
+    }
+}

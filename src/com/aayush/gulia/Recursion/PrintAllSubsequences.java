632c47432a020d29e55dd1dd696cfcd8f35a7c8f
Aayush Gulia <aayushgulia07@outlook.com>
2022-12-06 19:25:21 +0530

    docs edited.

--- src/com/aayush/gulia/Recursion/PrintAllSubsequences.java
+++ src/com/aayush/gulia/Recursion/PrintAllSubsequences.java
@@ -15,6 +15,8 @@ public class PrintAllSubsequences {
     /**
      * A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements,
      * but the order must be preserved.
+     * There are 2n-1 non-empty subsequences,
+     * Every subarray is a subsequence and every subset is a subsequence.
      *
      * @param index starting index of array.
      * @param arr sequence of elements.
@@ -37,7 +39,7 @@ public class PrintAllSubsequences {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
+        int[] arr = {1,2,3,4};
         List<ArrayList<Integer>> sequences = new ArrayList<>();
         allSubsequences(0, arr, new ArrayList<>(), sequences);
         System.out.println(sequences);

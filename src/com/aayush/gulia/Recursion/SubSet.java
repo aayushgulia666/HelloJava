560652f30603036b7e7a6b08c08bb9d3309d34c9
Aayush Gulia <aayushgulia07@outlook.com>
2022-12-06 22:23:53 +0530

    code revised for SubSet.java.

--- src/com/aayush/gulia/Recursion/SubSet.java
+++ src/com/aayush/gulia/Recursion/SubSet.java
@@ -2,13 +2,44 @@ package com.aayush.gulia.Recursion;
 
 import java.util.ArrayList;
 
+/**
+ * Subset -> not ordered and not contiguous.
+ * Subsequence -> ordered according to index and not contiguous.
+ */
 public class SubSet {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> allSubSets = new ArrayList<>();
         int[] arr = {1, 2, 2};
-        subSet(0, arr, new ArrayList<>(), allSubSets);
+        uniqueSubSet(0, arr, new ArrayList<>(), allSubSets);
         System.out.println(allSubSets);
     }
+
+    /**
+     * Generate unique subsets, subsequences
+     * @param index i
+     * @param arr array of elements
+     * @param ls temp array
+     * @param ds contains all subsets
+     */
+    static void uniqueSubSet(int index, int[] arr, ArrayList<Integer> ls, ArrayList<ArrayList<Integer>> ds){
+        ds.add(new ArrayList<>(ls));
+
+        for (int i=index; i< arr.length; i++){
+            if (i != index && arr[i] == arr[i-1]) continue;
+
+            ls.add(arr[i]);
+            uniqueSubSet(i+1, arr, ls, ds);
+            ls.remove(ls.size() - 1);
+        }
+    }
+
+    /**
+     * Generates all the subsets, subsequences.
+     * @param index i
+     * @param arr array
+     * @param ls temp ds
+     * @param ds stores answer
+     */
 
     static void subSet(int index, int[] arr, ArrayList<Integer> ls, ArrayList<ArrayList<Integer>> ds){
         if (index == arr.length){

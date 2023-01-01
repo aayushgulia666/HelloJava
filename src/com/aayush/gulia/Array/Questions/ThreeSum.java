32d90c05f4efbd2152c3f80450088e547cd8fe28
Aayush Gulia <aayushgulia07@outlook.com>
2023-01-01 15:14:09 +0530

    new methods to solve three sum added.
    Brute force method and method using hashmap added.

--- src/com/aayush/gulia/Array/Questions/ThreeSum.java
+++ src/com/aayush/gulia/Array/Questions/ThreeSum.java
@@ -1,8 +1,6 @@
 package com.aayush.gulia.Array.Questions;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Leet code 15
@@ -11,23 +9,121 @@ import java.util.List;
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
-        List<List<Integer>> ans = getTriplet(nums);
-        System.out.println(ans);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<ArrayList<Integer>> set = new HashSet<>();
+
+        getTriplet(nums, map, set);
+        System.out.println(set);
     }
 
+    /**
+     * Brute force approach, generate all triplets and check for the sum if sum is zero then add it to HashSet to avoid duplicates.
+     * all triplets will be stored in the HashSet passed while calling the method.
+     */
+    static void getTriplet(int[] nums, HashSet<ArrayList<Integer>> set){
+
+        for (int i=0; i<nums.length; i++){
+            for (int j=i+1; j< nums.length; j++){
+                for (int k=j+1; k< nums.length; k++){
+
+                    if (nums[i] + nums[j] + nums[k] == 0){
+
+                        ArrayList<Integer> ls = new ArrayList<>();
+                        Collections.addAll(ls, nums[i], nums[j], nums[k]);
+
+                        set.add(ls);
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Using HashMap (Hashing).
+     * will use HashMap to get the third triplet as a + b + c = 0   i.,e   c = -(a+b).
+     * @param nums array containing elements.
+     * @param map extra space to calculate third triplet.
+     * @param set contain triplets having sum 0.
+     */
+
+    static void getTriplet(int[] nums, HashMap<Integer, Integer> map, HashSet<ArrayList<Integer>> set){
+
+        //add all elements in the map with their occurrence
+        for (int i=0; i<nums.length; i++){
+
+            if (map.containsKey(nums[i])){
+                int count = map.get(nums[i]);
+                count++;
+                map.put(nums[i], count);
+                continue;
+            }
+
+            map.put(nums[i], 1);
+        }
+
+        //check for third triplet
+        for (int i=0; i<nums.length; i++){
+
+            if (map.containsKey(nums[i])){
+                int count = map.get(nums[i]);
+                count--;
+                map.put(nums[i], count);
+            }
+
+            for (int j=i+1; j< nums.length; j++){
+
+                if (map.containsKey(nums[j])){
+                    int count = map.get(nums[j]);
+                    count--;
+                    map.put(nums[j], count);
+                }
+
+                int thirdTriplet = -(nums[i] + nums[j]);
+
+                if (map.containsKey(thirdTriplet)){
+                    if (map.get(thirdTriplet) > 0){
+                        ArrayList<Integer> ls = new ArrayList<>();
+                        Collections.addAll(ls, nums[i], nums[j], thirdTriplet);
+                        Collections.sort(ls);
+
+                        set.add(ls);
+                    }
+                }
+
+                if (map.containsKey(nums[j])){
+                    int count = map.get(nums[j]);
+                    count++;
+                    map.put(nums[j], count);
+                }
+            }
+            if (map.containsKey(nums[i])){
+                int count = map.get(nums[i]);
+                count++;
+                map.put(nums[i], count);
+            }
+        }
+    }
+
+
+    /**
+     * Two pointer approach
+     * @param nums array containing elements
+     * @return triplets
+     */
     static List<List<Integer>> getTriplet(int[] nums){
         Arrays.sort(nums);
 
-        List<List<Integer>> ans = new LinkedList<>();
+                    List<List<Integer>> ans = new LinkedList<>();
 
-        for(int i=0; i<nums.length-2; i++){
+                    for(int i=0; i<nums.length-2; i++){
 
-            if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+                        if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
 
-                int lo = i+1, hi = nums.length-1, sum =  -nums[i];
+                            int lo = i+1, hi = nums.length-1, sum =  -nums[i];
 
-                while(lo < hi){
-                    if(nums[lo] + nums[hi] == sum){
+                            while(lo < hi){
+                                if(nums[lo] + nums[hi] == sum){
 //                        System.out.println(nums[i] + " " +  nums[lo] + " " + nums[hi]);
 
                         ans.add(Arrays.asList(nums[i], nums[lo], nums[hi]));

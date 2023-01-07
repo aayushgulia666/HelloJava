635251e4b768642fd6f6c18dac90f4d1b86a1d50
Aayush Gulia <aayushgulia07@outlook.com>
2023-01-07 11:18:57 +0530

    update balanced string method.

--- src/com/aayush/gulia/Strings/Questions/SplitStringInBalancedStrings.java
+++ src/com/aayush/gulia/Strings/Questions/SplitStringInBalancedStrings.java
@@ -3,40 +3,26 @@ package com.aayush.gulia.Strings.Questions;
 public class SplitStringInBalancedStrings {
     public static void main(String[] args) {
         String s = "RLRRRLLRLL";
-        System.out.println(balancedStringSplit(s));
+        System.out.println(balancedString(s));
     }
 
-    /**
-     * Returns maximum numbers of balanced strings.
-     * @param s String
-     * @return integer containing maximum number of balanced strings
-     */
-    static int balancedStringSplit(String s) {
-    int left = 0, right = 0, count = 0;
+    static int balancedString(String s){
+        int count = 0, balancedStrings = 0;
 
-    for(int i=0; i<s.length(); i++){
-        if(s.charAt(i) == 'L'){
-            if(right == 0){
-                left++;
-            }
-            else{
-                right--;
-            }
-        }
+        for (int i=0; i<s.length(); i++){
+            char current = s.charAt(i);
 
-            else{
-                if(left == 0){
-                    right++;
-            }
-            else{
-                left--;
-            }
-        }
-
-            if(left == 0 && right == 0){
+            if (current == 'R'){
                 count++;
+            }
+            if (current == 'L'){
+                count--;
+            }
+
+            if (count == 0){
+                balancedStrings++;
+            }
         }
-    }
-        return count;
+        return balancedStrings;
     }
 }
